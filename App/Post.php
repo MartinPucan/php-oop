834c839e976a9bf579cd04b2bdf54ddc5ae230a6
Martin Pucan <martin.pucan@pilulka.cz>
2020-06-09 16:16:09 +0200

    need remove - post

--- App/Post.php
+++ App/Post.php
@@ -1,9 +1,20 @@
 <?php
+declare(strict_types=1);
 
 namespace App;
 
-
 class Post
 {
-    
+    public function action()
+    {
+        return $this->data();
+    }
+
+    private function data()
+    {
+        return [
+            'dunno',
+            'good'
+        ];
+    }
 }
